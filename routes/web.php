a4a07ba8ba52a22387d35f011327bd6819b15b69
dewodewo31 <dewodewo3104@gmail.com>
2025-04-15 00:50:23 +0800

    Feat: adding color menu, crud page and method

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\ColorController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -11,4 +12,16 @@ Route::post('admin/auth', [AdminController::class, 'auth'])->name('admin.auth');
 Route::middleware('admin')->group(function () {
   Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin.index');
   Route::post('admin/logout', [AdminController::class, 'logout'])->name('admin.logout');
+
+  // colors
+  Route::resource('colors', ColorController::class, [
+    'names' => [
+      'index' => 'admin.colors.index',
+      'create' => 'admin.colors.create',
+      'store' => 'admin.colors.store',
+      'edit' => 'admin.colors.edit',
+      'update' => 'admin.colors.update',
+      'destroy' => 'admin.colors.destroy',
+    ]
+  ]);
 });
