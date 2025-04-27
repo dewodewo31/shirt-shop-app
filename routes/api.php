8d6549991e0b475326a37905a36b5007cd6f4888
dewodewo31 <dewodewo3104@gmail.com>
2025-04-28 06:07:06 +0800

    feat: api for user auth login

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,28 @@
 <?php
 
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
+Route::middleware('auth:sanctum')->group(function () {
+  Route::get('user', function (Request $request) {
+    return [
+      'user' => UserResource::make($request->user()),
+      'access_token' => $request->bearerToken()
+    ];
+  });
+  Route::post('user/logout', [UserController::class, 'logout']);
+  Route::put('user/profile/update', [UserController::class, 'UpdateUserProfile']);
+});
+// product
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/product/{color}/color', [ProductController::class, 'filterProductsByColor']);
 Route::get('/product/{size}/size', [ProductController::class, 'filterProductsBySize']);
 Route::get('/product/{searchTerm}/find', [ProductController::class, 'findProductsByTerm']);
 Route::get('/product/{product}/show', [ProductController::class, 'show']);
+
+// user
+Route::post('user/register', [UserController::class, 'store']);
+Route::post('user/login', [UserController::class, 'auth']);
